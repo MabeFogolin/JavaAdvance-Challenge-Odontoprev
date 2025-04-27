ed552aac3ecc89093593b0b82f6db55b57c7bb8a
mabe <mariafogolin08@gmail.com>
2025-04-27 12:06:59 -0300

    aaaaaa

--- N.I.B/src/main/java/com/fiap/N/I/B/configs/QueueConfig.java
+++ N.I.B/src/main/java/com/fiap/N/I/B/configs/QueueConfig.java
@@ -13,54 +13,54 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class QueueConfig {
 
-    @Bean
-    public Queue usuarioQueue() {
-        return QueueBuilder.durable("fila-usuario").build();
-    }
+    // @Bean
+    // public Queue usuarioQueue() {
+    //     return QueueBuilder.durable("fila-usuario").build();
+    // }
 
-    @Bean
-    public Exchange usuarioExchange() {
-        return ExchangeBuilder.directExchange("usuarioExchange").build();
-    }
+    // @Bean
+    // public Exchange usuarioExchange() {
+    //     return ExchangeBuilder.directExchange("usuarioExchange").build();
+    // }
 
-    @Bean
-    public Binding usuarioBinding(Queue usuarioQueue, Exchange usuarioExchange) {
-        return BindingBuilder.bind(usuarioQueue).to(usuarioExchange).with("routingKey").noargs();
-    }
+    // @Bean
+    // public Binding usuarioBinding(Queue usuarioQueue, Exchange usuarioExchange) {
+    //     return BindingBuilder.bind(usuarioQueue).to(usuarioExchange).with("routingKey").noargs();
+    // }
 
-    @Bean
-    public Queue profissionalQueue() {
-        return QueueBuilder.durable("fila-profissional").build();
-    }
+    // @Bean
+    // public Queue profissionalQueue() {
+    //     return QueueBuilder.durable("fila-profissional").build();
+    // }
 
-    @Bean
-    public Exchange profissionalExchange() {
-        return ExchangeBuilder.directExchange("profissionalExchange").build();
-    }
+    // @Bean
+    // public Exchange profissionalExchange() {
+    //     return ExchangeBuilder.directExchange("profissionalExchange").build();
+    // }
 
-    @Bean
-    public Binding profissionalBinding(Queue profissionalQueue, Exchange profissionalExchange) {
-        return BindingBuilder.bind(profissionalQueue).to(profissionalExchange).with("routingKey").noargs();
-    }
+    // @Bean
+    // public Binding profissionalBinding(Queue profissionalQueue, Exchange profissionalExchange) {
+    //     return BindingBuilder.bind(profissionalQueue).to(profissionalExchange).with("routingKey").noargs();
+    // }
 
-    @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        return objectMapper;
-    }
+    // @Bean
+    // public ObjectMapper objectMapper() {
+    //     ObjectMapper objectMapper = new ObjectMapper();
+    //     objectMapper.registerModule(new JavaTimeModule());
+    //     objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+    //     return objectMapper;
+    // }
 
-    @Bean
-    public Jackson2JsonMessageConverter messageConverter(ObjectMapper objectMapper) {
-        return new Jackson2JsonMessageConverter(objectMapper);
-    }
+    // @Bean
+    // public Jackson2JsonMessageConverter messageConverter(ObjectMapper objectMapper) {
+    //     return new Jackson2JsonMessageConverter(objectMapper);
+    // }
 
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
-                                         Jackson2JsonMessageConverter messageConverter) {
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(messageConverter);
-        return rabbitTemplate;
-    }
+    // @Bean
+    // public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+    //                                      Jackson2JsonMessageConverter messageConverter) {
+    //     RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+    //     rabbitTemplate.setMessageConverter(messageConverter);
+    //     return rabbitTemplate;
+    // }
 }
